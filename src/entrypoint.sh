35c2d059e65c136af608ab8e08d2967047c1df9c
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-12-31 13:14:02 +0100

    Print progress info

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Generating linear program.."
 ./generate_lp.py $1 $2 /tmp/problem.lp
+echo "Solving linear program.."
 lp_solve /tmp/problem.lp > /tmp/lp_result.txt
+echo "Evaluating result.."
 ./print_result_info.py $1 $2 /tmp/lp_result.txt > $3
+echo "Done"
